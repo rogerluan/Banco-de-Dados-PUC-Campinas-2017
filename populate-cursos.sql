3f8daf1f79ef4be7067e50e1cafab9b1e7df089e
Bruno Pedroso <pedrosoagt@gmail.com>
2017-05-09 14:56:12 -0300

    Add files via upload

---
+++ populate-cursos.sql
@@ -0,0 +1,3 @@
+INSERT INTO cursos (num,nome,local) VALUES (1,'engenharia de computacao','predio h6');
+INSERT INTO cursos (num,nome,local) VALUES (2,'engenharia eletrica','predio h8');
+INSERT INTO cursos (num,nome,local) VALUES (3,'engenharia de telecomunicacoes','ct a');
