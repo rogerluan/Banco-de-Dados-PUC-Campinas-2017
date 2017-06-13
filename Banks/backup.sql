e8b04ac0f75885cdc3d9fc77e6fa5364020efdba
Roger Oba <rogerluan.oba@gmail.com>
2017-06-13 15:15:07 -0300

    created backup script

---
+++ Banks/backup.sql
@@ -0,0 +1,72 @@
+create table backup_movimentos(
+	num_mov number,
+	num_conta number, 
+	num_agencia number,
+	data date,
+	tipo char,
+	valor number(15,2),
+	primary key(num_mov),
+	constraint num_conta_mov foreign key (num_conta) references contas(num_conta),
+	constraint num_agencia_mov foreign key (num_agencia) references agencias(num_agencia)
+);
+
+-- num_log = incremental uid
+-- chave = movimentos pk
+-- tipo = 'i', 'd' or 'u' (insert, delete or update)
+-- data = sysdate of insertion
+-- is_processed = 1 or 0 (true or false), representing whether it has already been processed or not
+
+create table log_movimentos(
+	num_log number,
+	chave number, 
+	tipo char,
+	data date,
+	is_processed number,
+	primary key(num_log),
+	constraint num_log_mov foreign key (num_log) references movimentos(num_mov)
+);
+
+-- log_movimentos sequence --
+create sequence inc_log start with 1 increment by 1;
+create or replace trigger trigger_inc_log
+before insert on log_movimentos
+for each row
+begin 
+	select inc_log.nextval into :new.num_log from dual;
+	select sysdate into :new.data from dual;
+	select 0 into :new.is_processed from dual;
+end;
+/
+
+create or replace trigger trigger_logger
+before insert or delete or update on movimentos
+for each row
+begin
+	if inserting then
+		insert into log_movimentos (chave, tipo) values (:new.num_mov, 'i');
+	elsif deleting then
+		insert into log_movimentos (chave, tipo) values (:old.num_mov, 'd');
+	elsif updating then
+		insert into log_movimentos (chave, tipo) values (:new.num_mov, 'u');
+	end if;
+end;
+/
+
+create or replace procedure update_backup is
+	line_exist number := 0;
+begin
+dbms_output.put_line ('Your backup is being processed...');
+for log in (select * from log_movimentos where is_processed = 0) loop
+	select count(*) into line_exist from backup_movimentos where num_mov = log.chave;
+	if line_exist = 0 then
+		insert into backup_movimentos (select * from movimentos where num_mov = log.chave); 
+	else
+		update backup_movimentos set (num_conta, num_agencia, data, tipo, valor) = (select num_conta, num_agencia, data, tipo, valor from movimentos where num_mov = log.chave) where num_mov = log.chave;
+	end if;
+	update log_movimentos set is_processed = 1 where num_log = log.num_log; 
+end loop;
+dbms_output.put_line ('Your backup is now up-to-date!');
+end;
+/
+
+set serveroutput on
